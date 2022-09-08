955afce75015defc7a24d0f8fcda8b0d5ff54593
jskodova <skodovajarmilatt@gmail.com>
2022-09-08 16:11:36 +0300

    adding explanations of functionality

--- forms.py
+++ forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import FlaskForm
-from main import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -30,11 +29,3 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
-def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 from flask_login import login_user, current_user, logout_user, login_required, UserMixin, LoginManager
-from datetime import datetime
+#SQL Alchemy is great for representing data from the db as models(classes) 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-import sqlite3
 from passlib.hash import bcrypt
 from forms import RegistrationForm, LoginForm
 
@@ -12,26 +11,28 @@ app = Flask(__name__, static_url_path='/static')
 
 #secret key is for app security to protect from attacks
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-#initializing classes SQLAlchemy and Bcrypt
+#Creating an instance of database
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+#Creating class User to hold information in the db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.email}', '{self.image_file}')"
@@ -51,15 +52,21 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
+    #taking RegistrationForm from forms.py
     form = RegistrationForm()
+    #Loop to check if the account has been successfuly generated, from SQLAlchemy
     if form.validate_on_submit():
+        #hashing the password that we get from Registrationform
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        #creating an instance of user class to input into the db
+        user = User(email=form.email.data, password=hashed_password)
+        #adding user to the db
         db.session.add(user)
+        #comitting session
         db.session.commit()
+        #flashing a notification about account creation
         flash('Your account has been created! You are now able to log in', 'success')
+        #taking user to login page after successful reg
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
